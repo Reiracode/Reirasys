c5314f3a62e71134bd560dd54c0d567cf8949e47
Reiracode <reira.igg@gmail.com>
2022-08-24 18:08:13 +0800

    Updates

---
+++ precache-manifest.2741f883bc8d229acf584ad1af3c1c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d8fe412220da2569980208d9fa1549",
+    "url": "/Reirasys/index.html"
+  },
+  {
+    "revision": "0d901d7b85370a17ec05",
+    "url": "/Reirasys/static/css/2.700f80b7.chunk.css"
+  },
+  {
+    "revision": "12baf0111076314a9563",
+    "url": "/Reirasys/static/css/main.4b5dfc6f.chunk.css"
+  },
+  {
+    "revision": "0d901d7b85370a17ec05",
+    "url": "/Reirasys/static/js/2.5ba0271b.chunk.js"
+  },
+  {
+    "revision": "12baf0111076314a9563",
+    "url": "/Reirasys/static/js/main.68c00627.chunk.js"
+  },
+  {
+    "revision": "d9e09a822743f8163fb4",
+    "url": "/Reirasys/static/js/runtime~main.ca2e499f.js"
+  }
+]);
